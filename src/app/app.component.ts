d86a954b357a102a10e5c1290c71aa66a1d0edcc
LukasComK <lukasthi7@gmail.com>
2024-03-17 12:28:44 -0300

    [P5F-5] Criação do component da tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InitialComponent } from './initial/initial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, InitialComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InitialComponent } from './initial/initial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': '', component: InitialComponent}
+];
