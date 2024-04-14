30cc42b7b712e6d00f33f7b9ba22214478836492
LukasComK <lukasthi7@gmail.com>
2024-04-14 17:25:38 -0300

    [P5F-5] Adicionado botão de login

--- src/app/initial/initial.component.ts
+++ src/app/initial/initial.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-initial',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './initial.component.html',
   styleUrl: './initial.component.scss'
 })
