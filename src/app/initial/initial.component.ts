8f2fd1cdc8c5d6ed8bc5eacea304c608cdd10d1c
Eduardo da Maia Haak <eduardodamaiahaak@gmail.com>
2024-04-26 20:30:21 -0300

    [P5F-5] Finalização da tela de Login

--- src/app/initial/initial.component.ts
+++ src/app/initial/initial.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @Component({
   selector: 'app-initial',
   standalone: true,
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './initial.component.html',
   styleUrl: './initial.component.scss'
 })
