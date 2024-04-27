fe0a8e295828e134be91fb69cfccebc2342bef57
Eduardo da Maia Haak <eduardodamaiahaak@gmail.com>
2024-04-27 01:42:38 -0300

    [P5F-6] Criado e Finalizado tela de registro, feitas algumas alterações na tela de login e estilos gerais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {'path': '', 
-    loadChildren: () => import('./initial/initial.module').then(m => m.InitialModule)}
+    loadChildren: () => import('./initial/initial.module').then(m => m.InitialModule)},
+    {'path': 'register', 
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)}
 ];
