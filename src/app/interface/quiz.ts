7fd9af1a2c6ce820dddcca192ff7d003f20d86e2
Eduardo da Maia Haak <eduardodamaiahaak@gmail.com>
2024-05-21 21:47:05 -0300

    [P5F-12] Criadas Interfaces categoria, quiz e user

---
+++ src/app/interface/quiz.ts
@@ -0,0 +1,19 @@
+type player_score = {
+    player: string;
+    score: number;
+};
+
+export interface Quiz {
+    name: string;
+    image: string;
+    description: string;
+    questions: Array<string>;        //['Pergunta 1?', 'Pergunta 2?', 'Pergunta 3?']
+    answers: Array<Array<string>>;   //[['Resposta 1A', 'Resposta 1B', 'Resposta 1C', 'Resposta 1D'], ['Resposta 2A', 'Resposta 2B', 'Resposta 2C', 'Resposta 2D'], ['Resposta 3A', 'Resposta 3B', 'Resposta 3C', 'Resposta 3D']]
+    correct_answers: Array<number>;  //[[1],[3],[2]]
+    times_played: number;
+    ranked_players: number;
+    favorites: number;
+    category: string;
+    tags: Array<string>;
+    top_scores: Array<player_score>; //[{ player: 'Player 1', score: 150 }, { player: 'Player2', score: 140 }, { player: 'Player3', score: 130 }]
+}
